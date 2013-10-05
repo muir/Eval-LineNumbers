afd761d5ffe7745a9ff4142d3bc0540d97f7d895
David Muir Sharnoff <muir@idiom.org>
2013-10-04 18:01:12 -0700

    - Complete makeover by Olivier Mengu?: 	- add caller-level support 	- add a test 	- fix typos, groom makefile and code
      restore MANIFEST
      add Travis
      add documentation for caller-levels
      bump version number

--- lib/Eval/LineNumbers.pm
+++ lib/Eval/LineNumbers.pm
@@ -7,8 +7,7 @@ use strict;
 use Exporter 5.57 'import';
 our @EXPORT_OK = qw(eval_line_numbers);
 
-our $VERSION = '0.32';
-$VERSION = eval $VERSION;
+our $VERSION = 0.33;
 
 sub eval_line_numbers
 {
@@ -35,6 +34,8 @@ Eval::LineNumbers - Add line numbers to heredoc blocks that contain perl source 
    code
  END_HEREIS
 
+ eval eval_line_numbers($caller_level, $code)
+
 =head1 DESCRIPTION
 
 Add a C<#line "this-file" 392> comment to heredoc/hereis text that is going
@@ -43,6 +44,22 @@ to be eval'ed so that error messages will point back to the right place.
 Please note: when you embed C<\n> in your code, it gets expanded in
 double-quote hereis documents so it will mess up your line numbering.
 Use C<\\n> instead when you can.
+
+=head2 Caller Level Example
+
+The second form of eval_line_numbers where a caller-level is provided
+is for the situation where the code is generated in one place and 
+eval'ed in another place.  The caller level should be the number of
+stack levels between where the heredoc was created and where it is
+eval'ed.
+
+ sub example {
+   return <<END_HEREIS
+     code
+END_HEREIS
+ }
+
+ eval eval_line_numbers(1, example())
 
 =head1 LICENSE
 
